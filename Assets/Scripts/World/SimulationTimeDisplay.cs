891d40456987f298ebc6582b5ae821167a845867
Tiago Marino <tmsilva11@gmail.com>
2022-04-27 09:19:23 +0200

    Add simulation time

---
+++ Assets/Scripts/World/SimulationTimeDisplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace ORCAS
+{
+    public class SimulationTimeDisplay : MonoBehaviour
+    {
+        [SerializeField] private TMPro.TextMeshProUGUI _text;
+        [SerializeField] private SimulationTimeController _timeController;
+
+        private void Update()
+        {
+            string time = (_timeController.GetSimulationTimeInHours() % 24).ToString("F2");
+            _text.SetText(time);
+        }
+    }
+}
