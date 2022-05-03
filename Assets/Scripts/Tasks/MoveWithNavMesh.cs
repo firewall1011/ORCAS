c2d80c3801b340ab3aee9d9c85eb41b909e97ee8
Tiago Marino <tmsilva11@gmail.com>
2022-05-03 15:55:54 +0200

    Add navmesh movement tasks for agents

---
+++ Assets/Scripts/Tasks/MoveWithNavMesh.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace ORCAS
+{
+    public class MoveWithNavMesh : Task
+    {
+        public readonly Transform target;
+
+        public MoveWithNavMesh(Transform target)
+        {
+            this.target = target;
+        }
+
+        public override IEnumerator Perform(GameObject agent)
+        {
+            if (agent.TryGetComponent(out UnityEngine.AI.NavMeshAgent navAgent))
+            {
+                if (!navAgent.SetDestination(target.position))
+                {
+                    InvokeOnExecutionEnded(false);
+                    yield break;
+                }
+
+                while(navAgent.remainingDistance > float.Epsilon)
+                {
+                    yield return null;
+                }
+                
+                InvokeOnExecutionEnded(true);
+            }
+            else
+            {
+                InvokeOnExecutionEnded(false);
+            }
+        }
+    }
+}
