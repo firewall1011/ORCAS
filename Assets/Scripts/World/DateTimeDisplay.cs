0e3912f159c1dfcae5a6cfc6c70751fb1323db16
Tiago Marino <tmsilva11@gmail.com>
2022-05-13 12:19:04 +0200

    Add date time manager

---
+++ Assets/Scripts/World/DateTimeDisplay.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace ORCAS
+{
+    public class DateTimeDisplay : MonoBehaviour
+    {
+        [SerializeField] private TMPro.TextMeshProUGUI displayMesh;
+
+        private DateTimeManager _timeManager;
+
+        private void Awake()
+        {
+            _timeManager = FindObjectOfType<DateTimeManager>();
+        }
+
+        private void Update()
+        {
+            string text = _timeManager.DateTime.ToShortTimeString();
+
+            displayMesh.SetText(text);
+        }
+    }
+}
---
+++ Assets/Scripts/World/DateTimeManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace ORCAS
+{
+    public class DateTimeManager : MonoBehaviour
+    {
+        public int Hour => _dateTime.Hour;
+        public int Minute => _dateTime.Minute;
+        public DateTime DateTime => _dateTime;
+
+        [SerializeField, Min(0.01f), Tooltip("How many real life seconds are there in a simulation minute")] 
+        private float _secondsPerMinute = 0.5f;
+        private DateTime _dateTime;
+        private float _timer = 0f;
+
+        private void Start()
+        {
+            _dateTime = DateTime.UtcNow;
+        }
+
+        private void Update()
+        {
+            _timer += Time.deltaTime;
+
+            if (_timer >= _secondsPerMinute)
+            {
+                _dateTime = _dateTime.AddMinutes(1);
+                _timer -= _secondsPerMinute;
+            }    
+        }
+    }
+}
